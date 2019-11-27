9bbad14951099853c44407dd82220c7cca10ae57
ricardoivalle <56512487+ricardoivalle@users.noreply.github.com>
2019-11-27 15:39:45 -0300

    Update ServiceHelper.cs

--- ExternalServices/ServiceHelper.cs
+++ ExternalServices/ServiceHelper.cs
@@ -71,7 +71,7 @@ namespace ExternalServices
         /// <param name="JsonParams"></param>
         /// <param name="timeOut"></param>
         /// <returns></returns>
-        public static HttpResponseMessage Get(String URL, int timeOut = DEFAULT_TIMEOUT, string extraParam = null)
+        public static HttpResponseMessage Get(String URL, int timeOut = DEFAULT_TIMEOUT, string myParam = null)
         {
             var response = new HttpResponseMessage();
             response.StatusCode = HttpStatusCode.NotFound;
